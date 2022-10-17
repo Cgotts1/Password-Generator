e4605898e328f9c360f07697edff90a472a5c7bf
Cgotts <cobi.gottschalk@gmail.com>
2022-10-16 19:52:12 -0700

    Cleaned up code and commented on certain section's functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var specialCharacter = "~`!@#$%^&*()-_+={}[]|/\:;\"'<>,.?"; // Contains full string of characters
-var specCharSplit = specialCharacter.split('');             //Turns string to an array
+var specCharSplit = specialCharacter.split('');             // Turns string to an array
 
 var numericCharacter = "0123456789";
 var numCharSplit = numericCharacter.split('');              
@@ -10,12 +10,7 @@ var upCaAlCharSplit = upperCaseAlphabet.split('');
 var lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
 var lowCaAlCharSplit = lowerCaseAlphabet.split('');         
 
-
-
-
-
 // Functions that return random desired characters https://www.coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
-
 function randomSpecialCharacter(){
   return specCharSplit[Math.floor(Math.random() * specCharSplit.length)];        
 }
@@ -32,16 +27,10 @@ function randomLowerCaseLetter(){
   return lowCaAlCharSplit[Math.floor(Math.random() * lowCaAlCharSplit.length)];  
   }
   
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
 // Function for generating password below
-
 function generatePassword(){
   
 var results = [];  
@@ -85,7 +74,7 @@ var shuffled = "";
 
 var counter = 0;
 
-for(var i = 0; i <= passwordLength; i++){               //Removed -1 from passwordLength to return exact # of characters for user input
+for(var i = 0; i <= passwordLength - 1; i++){         //Added -1 to passwordLength to return exact # of characters for user input
   var randomCharacter = "";
   
   if(counter==0) {
@@ -156,27 +145,15 @@ for(var i = 0; i <= passwordLength; i++){               //Removed -1 from passwo
       randomCharacter = randomLowerCaseLetter()
       finalPassword += randomCharacter
     }
-  }                                                                                               //end of forloop
-
-
-
-
-  // https://www.youtube.com/watch?v=dlTL6fNu4Po
-
-
-
-
-  
+  }                                              //end of forloop
 
   if(counter==3) counter=0
   else counter++;
 
 } // For
 
-
-function shuffledString(finalPassword){                                    //function for shuffling string
-
-  var shuffled = "";                                          //Stores shuffled string                                                          
+function shuffledString(finalPassword){          // Function for shuffling string
+  var shuffled = "";                             // https://www.youtube.com/watch?v=dlTL6fNu4Po                                                                                                
   finalPassword = finalPassword.toString();                                                        
   len = finalPassword.length;                                                                       
   for(i=0; i<len; i++){                                                                                 
@@ -192,86 +169,18 @@ function shuffledString(finalPassword){                                    //fun
 
 
  
-  return finalPassword                //Places the finalPassword into textbox on webpage
+  return finalPassword                                    // Places the finalPassword into textbox on webpage
 
-} //generatePassword
+} // End of generatePassword()
 
-// Write password to the #password input
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordTextBox = document.querySelector("#password");
 
-  passwordTextBox.value = password;                       /* Inserts password text   */
+  passwordTextBox.value = password;                       // Inserts password text  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);                    /* Enables random password to generate when button clicked  */
-
-
-
-
-
-
-
-
-// var shuffle = s.split('').sort(() => 0.5 - Math.random()).join('');                       https://www.encodedna.com/javascript/how-to-shuffle-characters-in-a-string.htm
-
-
-// function shuffledString(string){                                                          https://www.youtube.com/watch?v=dlTL6fNu4Po
-
-//   var shuffled = "";                       //Stores shuffled string
-//   string = string.toString();
-//   len = string.length;
-//   for(i=0; i<len; i++){
-//     randPos = Math.floor(Math.random() * string.length);
-//     shuffled += string[randPos];
-//     string = string.slice(0, randPos) + string.slice(randPos + 1)
-//   }
-//   return shuffled;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function shuffledString(){
-
-//   var shuffled = "";                       //Stores shuffled string                                                          
-//   finalPassword = finalPassword.toString();                                                               Makes sure input is a string
-//   len = finalPassword.length;                                                                             gets the length of the string
-//   for(i=0; i<len; i++){                                                                                   transfers the characters of the original sting to new string in random order
-//     randomPosition = Math.floor(Math.random() * finalPassword.length);
-//     shuffled += finalPassword[randomPosition];                                                            adds random character to the new string
-//     finalPassword = finalPassword.slice(0, randomPosition) + finalPassword.slice(randomPosition + 1)      removes random character from original string
-//   }
-//   return shuffled;
-// }
-
-// var shuffled = shuffledString()
